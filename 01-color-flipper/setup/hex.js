7e35c1ecd5858c1cf25183251dbe2d85df2fe21a
MauFerrusca <mferrusca@gmail.com>
2023-09-06 15:14:01 -0600

    color flipper updates

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById('btn');
+const colorSpan = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+  let hexColor = '#';
+  for (let i = 0; i < 6; i++) {
+    console.log(getRandomNumber());
+    hexColor += hex[getRandomNumber()];
+  }
+  colorSpan.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+  // console.log(hexColor);
+})
+
+function getRandomNumber() {
+  // return Math.floor(Math.random() * hex.length)
+  return Math.floor(Math.random() * hex.length);
+}
